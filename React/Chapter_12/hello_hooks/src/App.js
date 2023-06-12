d62a9a7a0cbecc6d2d6e0e22ba6ad9d7088413b2
Linda den Hoed <aidan.denhoed@gmail.com>
2023-06-12 14:17:18 +0200

    first commit

---
+++ React/Chapter_12/hello_hooks/src/App.js
@@ -0,0 +1,164 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Class Variant
+// import './App.css';
+// import React, { useState } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Button } from "react-bootstrap";
+
+// class App extends React.Component {
+// state = { requested: '' };
+//   render() {
+
+//   return (
+//     <div>
+//       <Button variant="link" onClick={() => this.setState({
+//         requested: "https://jsonplaceholder.typicode.com/posts"})}>
+//         Posts
+//       </Button>
+//       <Button variant="link" onClick={() => this.setState({
+//         requested: "https://jsonplaceholder.typicode.com/todos"})}>
+//         Todos
+//       </Button>
+//       <br />
+//       Requested: {this.state.requested}
+//     </div>
+//   );
+//   }
+//   };
+
+// export default App;
+
+// Function Variant
+// import "./App.css";
+// import React, { useState, useEffect } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Button } from "react-bootstrap";
+
+// const App = () => {
+//   const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+//   const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+
+//   const [requested, setRequested] = useState(postsUrl);
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     fetch(requested)
+//       .then((response) => response.json())
+//       .then((data) => setData(data));
+//   }, []);
+
+//   return (
+//     <div>
+//       <Button variant="link" onClick={() => setRequested(postsUrl)}>
+//         Posts
+//       </Button>
+//       <Button variant="link" onClick={() => setRequested(todosUrl)}>
+//         Todos
+//       </Button>
+//       <br />
+//       Requested: {requested}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import "./App.css";
+// import React, { useState, useEffect } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Button } from "react-bootstrap";
+
+// const App = () => {
+//   const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+//   const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+
+//   const [requested, setRequested] = useState(postsUrl);
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     fetch(requested)
+//       .then((response) => response.json())
+//       .then((data) => setData(data));
+//   }, [requested]);
+
+//   return (
+//     <div>
+//       <Button variant="link" onClick={() => setRequested(postsUrl)}>
+//         Posts
+//       </Button>
+//       <Button variant="link" onClick={() => setRequested(todosUrl)}>
+//         Todos
+//       </Button>
+//       <br />
+//       Requested: {requested}
+//       <ul>
+//         {data.map((el) => (
+//           <li key={el.id}>{el.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import useFetch from "./useFetch";
+import Users from "./Users";
+
+const App = () => {
+  const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+
+  const [requested, setRequested] = useState(postsUrl);
+  const data = useFetch(requested);
+
+  return (
+    <div>
+      <Button variant="link" onClick={() => setRequested(postsUrl)}>
+        Posts
+      </Button>
+      <Button variant="link" onClick={() => setRequested(todosUrl)}>
+        Todos
+      </Button>
+      <Users />
+      <br />
+      Requested: {requested}
+      <ul>
+        {data.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;

e0f2afa2d02bc756e89a1f0102c066c7c83dd5a3
Linda den Hoed <aidan.denhoed@gmail.com>
2023-06-14 17:26:38 +0200

    first commit

---
+++ React/Projects/Project_5/react-18-shopping-cart/src/App.js
@@ -0,0 +1,81 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Basket from "./components/Basket";
+import data from "./data";
+import { useDeferredValue, useEffect, useState, useTransition } from "react";
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const { products } = data;
+  // Add function
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      const newCartItems = cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+      );
+      // important for updating qty
+      setCartItems(newCartItems);
+      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+    } else {
+      const newCartItems = [...cartItems, { ...product, qty: 1 }];
+      // important for updating qty
+      setCartItems(newCartItems);
+      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+    }
+  };
+  // Remove function
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      const newCartItems = cartItems.filter((x) => x.id !== product.id);
+      // important for updating qty
+      setCartItems(newCartItems);
+      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+    } else {
+      const newCartItems = cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+      );
+      // important for updating qty
+      setCartItems(newCartItems);
+      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+    }
+  };
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(() => {
+      setCartItems(
+        localStorage.getItem("cartItems")
+          ? JSON.parse(localStorage.getItem("cartItems"))
+          : []
+      );
+    })
+  }, []);
+
+  const cartItemsCount = useDeferredValue(cartItems.length);
+
+  return isPending ? (<div>Loading...</div>) 
+  : (
+    <div>
+      <Header countCartItems={cartItemsCount} />
+      <div className="row">
+        <Main
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          products={products}
+        />
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          products={products}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Projects/Project_5/react-18-shopping-cart/src/data.js
@@ -0,0 +1,26 @@
+const data = {
+    products: [
+        {
+            id:'1',
+            name:'Macbook',
+            price:1400,
+            image:'./images/MacbookPro13.webp'
+        },
+
+        {
+            id:'2',
+            name:'Old car',
+            price:2400,
+            image:'./images/Old_car.jpg'
+        },
+
+        {
+            id:'3',
+            name:'W.Shoes',
+            price:1000,
+            image:'./images/Shoes.webp'
+        },
+    ],
+}
+
+export default data;

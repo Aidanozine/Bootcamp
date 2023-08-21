b7aa149b18c3ea7a70a3a483f952cd918571e580
Linda den Hoed <aidan.denhoed@gmail.com>
2023-08-21 10:25:16 +0200

    first commit

--- Java/JavaGroupA/src/Exercises/Lesson_9/Ex_2/ShoppingCart.java
+++ Java/JavaGroupA/src/Exercises/Lesson_9/Ex_2/ShoppingCart.java
@@ -5,8 +5,8 @@ package Exercises.Lesson_9.Ex_2;
 public class ShoppingCart {
     public static void main(String args[]){
 
-        Customer cust1 = new Customer("Jack Daniels", "084-123-4567");
+        Customer a = new Customer("Jack Daniels", "084-123-4567");
 
-        System.out.println("Customer name: "+cust1.getName());
+        System.out.println("Customer name: "+a.getName());
     }    
 }

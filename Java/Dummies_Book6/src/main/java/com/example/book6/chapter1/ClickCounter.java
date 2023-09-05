acfaceed8ec83f2fb7e5f83602da75864632518c
Linda den Hoed <aidan.denhoed@gmail.com>
2023-09-05 12:15:52 +0200

    first commit

---
+++ Java/Dummies_Book6/src/main/java/com/example/book6/chapter1/ClickCounter.java
@@ -0,0 +1,47 @@
+package com.example.book6.chapter1;
+
+import javafx.application.Application;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.*;
+import javafx.stage.*;
+
+public class ClickCounter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    Button btn;
+    Label lbl;
+    int iClickCount = 0;
+
+    @Override
+    public void start(Stage primaryStage) {
+        btn = new Button();
+        btn.setText("CLick Here!");
+        btn.setOnAction(e -> buttonClick());
+
+        lbl = new Label();
+        lbl.setText("You still need to click 'da button.");
+
+        BorderPane pane = new BorderPane();
+        pane.setTop(lbl);
+        pane.setCenter(btn);
+
+        Scene scene = new Scene(pane, 250, 150);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Let's Count The Clicks!");
+        primaryStage.show();
+    }
+
+    public void buttonClick(){
+        iClickCount++;
+        if (iClickCount == 1){
+            lbl.setText("You've Clicked Once!");
+        } else {
+            lbl.setText("You've Clicked " + iClickCount + " Times!");
+        }
+    }
+}
---
+++ Java/Dummies_Book6/src/main/java/com/example/book6/chapter2/AddSubtract1.java
@@ -0,0 +1,16 @@
+package com.example.book6.chapter2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AddSubtract1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
